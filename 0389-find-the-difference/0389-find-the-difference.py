9c7f947fadb8e3482378d013fa6ea74a32ca0200
Shreeya Nayak <142816058+Shr-ee-ya@users.noreply.github.com>
2025-02-01 00:38:39 +0530

    Time: 1 ms (76.2%), Space: 12.4 MB (42.99%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        result = 0
+        for char in s + t:
+            result ^= ord(char)
+        return chr(result)
+        
+        '''
+        sin=set()
+        n=max(len(s),len(t))
+        for i in range(n):
+            if i<len(s) and s[i] not in t:
+                sin.add(s[i])
+            if i<len(t) and t[i] not in s:
+                sin.add(t[i])
+        return "".join(sin)
+        '''
