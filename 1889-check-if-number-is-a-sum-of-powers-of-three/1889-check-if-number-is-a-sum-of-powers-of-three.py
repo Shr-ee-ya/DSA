39df376e0fac6936e2e3127ba59ca1d4dcff0436
Shreeya Nayak <142816058+Shr-ee-ya@users.noreply.github.com>
2025-03-04 14:46:25 +0530

    Time: 0 ms (100%), Space: 12.5 MB (55%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,9 +1,22 @@
 class Solution(object):
     def checkPowersOfThree(self, n):
+        if n==1:
+            return True
+
+        if n % 3==0:
+           return self.checkPowersOfThree(n/3)
+
+        if (n - 1) % 3 == 0:
+            return self.checkPowersOfThree((n - 1)/3)
+
+        return False
+
+        '''
         while n>0:
             if n%3==2: #its a base term because when rem=2 then we have to take atleast 2 
             # similar powers of 3 to make the sum
                 return False
             n=n//3
         return True
+        '''
         
